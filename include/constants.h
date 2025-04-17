5600f8ea9cb041cac3386dc66755b914ae8ee7c5
Karman <u38_markar_lbn@technischools.com>
2025-04-17 14:45:37 +0200

    initialization and constants

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include<string>
+#include<vector>
+
+#define ISBN_LENGTH 13
+#define MAX_USERS 1000
+typedef int UserID;
+
+const std::string DEAFULT_DATA_PATH = "./data/";
+const int MAX_RENTAL_PERIOD = 14;
